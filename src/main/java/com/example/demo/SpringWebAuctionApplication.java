e07dc08ce4227255490c57ff4f41aefbd10c71f0
8025kento <8025kento@gmail.com>
2019-12-17 17:21:10 +0900

    コミットテスト

---
+++ src/main/java/com/example/demo/SpringWebAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAuctionApplication.class, args);
+	}
+
+}
